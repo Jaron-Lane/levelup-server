d9267996ce9f9dfec4a7fdd09f50b0b7d575801e
Jaron-Lane <jaronwohlwend@icloud.com>
2021-02-05 15:14:42 -0600

    initial commit

---
+++ levelupapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .gamer import Gamer
+from .gametype import GameType
+from .event import Event
+from .game import Game
+from .eventgamers import EventGamers
---
+++ levelupapi/models/game.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Game(models.Model):
+    title = models.CharField(max_length=75)
+    gametype = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    number_of_players = models.IntegerField()
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
