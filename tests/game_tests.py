3caf8c716c6f570092acd81f1fad7165f5462dac
Jaron-Lane <jaronwohlwend@icloud.com>
2021-03-01 14:41:58 -0600

    Created GET test for games

--- tests/game_tests.py
+++ tests/game_tests.py
@@ -1,7 +1,7 @@
 import json
 from rest_framework import status
 from rest_framework.test import APITestCase
-from levelupapi.models import GameType
+from levelupapi.models import GameType, Game
 
 class GameTests(APITestCase):
     def setUp(self):
@@ -47,9 +47,9 @@ class GameTests(APITestCase):
         data = {
             "title": "Clue",
             "numberOfPlayers": 6,
-            # "skillLevel": 5,
             "description": "Its a mysterious game",
-            "gameTypeId": 1
+            "gameTypeId": 1,
+            "gamer": 1
         }
 
         # Make sure request is authenticated
@@ -67,6 +67,40 @@ class GameTests(APITestCase):
         # Assert that the properties on the created resource are correct
         self.assertEqual(json_response["title"], "Clue")
         self.assertEqual(json_response["description"], "Its a mysterious game")
-        # self.assertEqual(json_response["skill_level"], 5)
         self.assertEqual(json_response["number_of_players"], 6)
         self.assertEqual(json_response["gametype"]["id"], 1)
+        self.assertEqual(json_response["gamer"]["id"], 1)
+
+    def test_get_game(self):
+        """
+        Ensure we can get an existing game.
+        """
+
+        # Seed the database with a game
+        game = Game()
+        game.title = "Monopoly"
+        game.gametype_id = 1
+        game.number_of_players = 4
+        game.gamer_id = 1
+        game.description = "A really good time if you win"
+
+        game.save()
+
+        # Make sure request is authenticated
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+
+        # Initiate request and store response
+        response = self.client.get(f"/games/{game.id}")
+
+        # Parse the JSON in the response body
+        json_response = json.loads(response.content)
+
+        # Assert that the game was retrieved
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Assert that the values are correct
+        self.assertEqual(json_response["title"], "Monopoly")
+        self.assertEqual(json_response["gametype"]["id"], 1)
+        self.assertEqual(json_response["number_of_players"], 4)
+        self.assertEqual(json_response["gamer"]["id"], 1)
+        self.assertEqual(json_response["description"], "A really good time if you win")
